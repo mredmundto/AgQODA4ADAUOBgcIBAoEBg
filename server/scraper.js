75fd6326ef1172973f52ce0b825523f0c2edfe6b
mredmundto <yatlunto@gmail.com>
2016-09-22 01:14:33 +0800

    web scraper

--- server/scraper.js
+++ server/scraper.js
@@ -1,7 +1,18 @@
+var request = require('request'); 
+var cheerio = require('cheerio'); 
 
 var scraper = function(){
-	console.log('inside scraper'); 
+	url = 'http://www.xe.com';
+	request(url, function(error, response, html){
+		var arr = []; 
+		var $ = cheerio.load(html); 
+		$('.rateCell').map(function(){
+			var pair = $(this).children().attr('rel').replace(/,|[0-9]/g, ""); 
+			var rate = $(this).text().replace(/\t+/g, "")
+			arr.push([pair,rate]); 
+		})
+		console.log(arr)
+	})
 
-	
 }
 module.exports = scraper; 
