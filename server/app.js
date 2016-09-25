43c02289c24d64ffc0051e36d3cfd172a48cf76c
mredmundto <yatlunto@gmail.com>
2016-09-25 20:21:23 +0800

    add co and add mongoDB

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ console.log('node server here')
 
 // run mongod in command line 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://USERNAME:PASSWORD@ds041586.mlab.com:41586/aftership-mredmundto');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
