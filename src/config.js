77beaa7bfac09cf377034c9b0bc3f0c5b8e69908
mredmundto <yatlunto@gmail.com>
2016-10-05 22:44:43 +0800

    changed beanstalkd address and db to production

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 'use strict';
 const config = {
 	beanstalkd: {
-		//address: 'challenge.aftership.net',
-		address: 'localhost',
+		address: 'challenge.aftership.net',
+		//address: 'localhost',
 		port: 11300,
 		tubename: 'mredmundto'
 	}
--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
 'use strict';
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('localhost');
-//mongoose.connect('mongodb://mredmundto:Yatlun85@ds041586.mlab.com:41586/aftership-mredmundto');
+//mongoose.connect('localhost');
+mongoose.connect('mongodb://mredmundto:Yatlun85@ds041586.mlab.com:41586/aftership-mredmundto');
 // define database schema
 let Rate = mongoose.model('Rate', {
 	from: String,
